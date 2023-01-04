a22bd8a238718621226a613a4ce33f923b6691d1
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-01-04 13:39:16 -0500

    Add files via upload

---
+++ data.ts
@@ -0,0 +1,154 @@
+
+export const portfolio = {
+  about: {
+    firstName: "Fren",
+    lastName: "Tech",
+    img: '/4567.png',
+    bio: `Hi, I'm Bankkroll! 
+
+I began my development journey years ago and have committed myself to building products that help WEB2 & WEB3.
+
+I have considerable experience with HTML, CSS, Python, Typescript, Javascript, Next, React, Node, Tailwind, Java, Bootstrap, Jquery, Vercel, Netlify, Ethereum, Solana, Twitter, Discord, Web3.
+
+I also have experience with solidity and rust and have developed dozens of smart contracts for ethereum/solana.
+
+I recently started "Fren Tech" to help design and build products and applications that are accessible and easily functional to everyone.
+
+Specializing in Dapps, Websites, Token Gating, Contracts, and more!`,
+    skills: ["HTML", "CSS", "Python", "Typescript", "Javascript", "Next", "React", "Node", "Tailwind", "Java", "Bootstrap", "Jquery", "Solidity", "Rust", "Vercel", "Netlify", "Ethereum", "Solana", "Twitter", "Discord", "Web3"],
+  },
+  projects: [
+    {
+      name: "GitHub",
+      img: '/tmk.png',
+      tools: ["HTML", "CSS", "Python", "Typescript", "Javascript", "Next", "React", "Node", "Tailwind", "Java", "Bootstrap", "Jquery", "Solidity", "Rust", "Vercel", "Netlify", "Ethereum", "Solana", "Twitter", "Discord", "Web3"],
+      url: "https://github.com/BankkRoll",
+      description: `Here you can view my github and all my open source Projects!`,
+    },
+
+
+    {
+      name: "Server Inspector",
+      img: '/server.png',
+      tools: ["Discord.js", "TypeScript"],
+      url: "https://serverinspector.xyz/",
+      description: `Server Inspectorᴮᴱᵀᴬ is here to monitor your server from bots, links , and other security risks!
+
+Multi-step verification panel with buttons, modals, and a in server 3 step panel. Contains advanced DM reminder for protection & advanced setup for bot defence. Even set up a lobby password for your verify panel!
+
+Configure raid protection, user names to be kicked, links to be blocked and many more! Set custom parameters for your server.
+
+Bot token is occasionally reset on updates. Bot code is only owned by one person and never shared.`,
+    },
+
+
+    {
+      name: "Alien Evo Sales Bot",
+      img: '/evobotfren.png',
+      tools: ["Discord.js", "TypeScript"],
+      url: "https://serverinspector.xyz/",
+      description: `Incubator burns | Evo sales | Ufo sales | and Frens originals sales | Founder:  @_masoncrowe | #alienfrens #isyoufren`,
+    },
+
+
+    {
+      name: "Frens Radio Network",
+      img: '/frensradio.png',
+      tools: ["React", "Bootstrap"],
+      url: "https://www.frensradio.io/",
+      description: `Frens Radio Network provides a global platform for all Web3 to pool skills, resources, opportunities for growth, professional expertise, entrepreneurial knowledge and have a great time! 
+
+Now with the Frens Radio Network you can not only find all your favourite shows in one place and tune in for great topics, but you can get updates on giveaways, find links to all your favourite hosts content, book a show and more.
+
+As a holder of the Frens Radio NFT you also have access to the Frens Radioverse where the community can apply for builder’s grants that are decided on by the community. NFT holders can also take full advantage of our network of likeminded business professionals thru FREN CONNECT and grow their presence faster.`,
+    },
+
+    {
+      name: "Frens Connect",
+      img: '/soon.gif',
+      tools: ["Javascript", "Next", "React", "Node", "web3.js", "Animate.css"],
+      url: "https://www.app.frensradio.io/",
+      description: `A new type of platform for connecting web3.
+
+All participants in Frens Connect can...
+
+• Easily find professionals and builders in the space & supercharge their networking opportunities by filtering exactly what they're looking for.
+
+• Be selected for grant opportunities to put toward their business or project.
+
+• Select builders and projects that will receive the grant funds through our polling section.
+
+• Post and find jobs in WEB3!
+
+• Enter holder only giveaways.`,
+    },
+
+    {
+      name: "Frenship Book Co.",
+      img: '/frenshipbook.gif',
+      tools: ["React", "Bootstrap"],
+      url: "https://www.frenshipbookcompany.com/",
+      description: `The first book in our series of children literature that aims to inspire and guide the next generation of Frens throughout the world.
+
+Join Blooey and many other endearing characters as they journey through the universe learning about perseverance, kindness, and many other important values.`,
+    },
+
+    {
+      name: "Cheddar & Frens",
+      img: '/cheddar.gif',
+      tools: ["React", "Bootstrap"],
+      url: "https://www.frenshipbookcompany.com/",
+      description: ``,
+    },
+
+
+    {
+      name: "",
+      img: '/',
+      tools: ["HTML", "CSS", "Python", "Typescript", "Javascript", "Next", "React", "Node", "Tailwind", "Java", "Bootstrap", "Jquery", "Solidity", "Rust", "Vercel", "Netlify", "Ethereum", "Solana", "Twitter", "Discord", "Web3"],
+      url: "",
+      description: ``,
+    },
+
+    {
+      name: "",
+      img: '/',
+      tools: ["HTML", "CSS", "Python", "Typescript", "Javascript", "Next", "React", "Node", "Tailwind", "Java", "Bootstrap", "Jquery", "Solidity", "Rust", "Vercel", "Netlify", "Ethereum", "Solana", "Twitter", "Discord", "Web3"],
+      url: "",
+      description: ``,
+    },
+
+    {
+      name: "",
+      img: '/',
+      tools: ["HTML", "CSS", "Python", "Typescript", "Javascript", "Next", "React", "Node", "Tailwind", "Java", "Bootstrap", "Jquery", "Solidity", "Rust", "Vercel", "Netlify", "Ethereum", "Solana", "Twitter", "Discord", "Web3"],
+      url: "",
+      description: ``,
+    },
+
+    {
+      name: "",
+      img: '/',
+      tools: ["HTML", "CSS", "Python", "Typescript", "Javascript", "Next", "React", "Node", "Tailwind", "Java", "Bootstrap", "Jquery", "Solidity", "Rust", "Vercel", "Netlify", "Ethereum", "Solana", "Twitter", "Discord", "Web3"],
+      url: "",
+      description: ``,
+    },
+
+    {
+      name: "",
+      img: '/',
+      tools: ["HTML", "CSS", "Python", "Typescript", "Javascript", "Next", "React", "Node", "Tailwind", "Java", "Bootstrap", "Jquery", "Solidity", "Rust", "Vercel", "Netlify", "Ethereum", "Solana", "Twitter", "Discord", "Web3"],
+      url: "",
+      description: ``,
+    },
+
+    {
+      name: "",
+      img: '/',
+      tools: ["HTML", "CSS", "Python", "Typescript", "Javascript", "Next", "React", "Node", "Tailwind", "Java", "Bootstrap", "Jquery", "Solidity", "Rust", "Vercel", "Netlify", "Ethereum", "Solana", "Twitter", "Discord", "Web3"],
+      url: "",
+      description: ``,
+    },
+
+  ],
+};
